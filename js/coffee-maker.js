812064de9974eb0be21bd46c3bd9b2de51087a35
valtykhoniuk <206243967+valtykhoniuk@users.noreply.github.com>
2025-04-19 13:53:33 +0200

    added: coffee-maker page

---
+++ js/coffee-maker.js
@@ -0,0 +1,11 @@
+const selectedDrink = document.getElementById("drink-select");
+const buttonMakeDrink = document.getElementById("make-drink");
+const statusDrinkText = document.getElementById("status-text");
+
+buttonMakeDrink.addEventListener("click", () => {
+  const drink = selectedDrink.value;
+  statusDrinkText.textContent = `Status: Making ${drink}..`;
+  setTimeout(() => {
+    statusDrinkText.textContent = `Status: Your ${drink} is ready`;
+  }, 4000);
+});
